a082b727f0d5295bad4eefa185aa3a89ee2162df
Fuad Makarim <m.fuadmakarim@gmail.com>
2022-10-31 07:57:09 +0700

    firebase signin google

--- App.js
+++ App.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import {SafeAreaView, Text, TouchableOpacity, View} from 'react-native';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+} from '@react-native-google-signin/google-signin';
+import auth from '@react-native-firebase/auth';
+import {GOOGLE_CLIENT_ID} from '@env';
 
-const signInGoogle = () => {
-  console.log('test');
+GoogleSignin.configure({
+  webClientId: GOOGLE_CLIENT_ID,
+});
+
+const signInGoogle = async () => {
+  try {
+    let {idToken} = await GoogleSignin.signIn();
+
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const App = () => {
@@ -14,6 +34,11 @@ const App = () => {
             <Text>Sign In with Google</Text>
           </View>
         </TouchableOpacity>
+        <GoogleSigninButton
+          size={GoogleSigninButton.Size.Wide}
+          color={GoogleSigninButton.Color.Dark}
+          onPress={signInGoogle}
+        />
       </View>
     </SafeAreaView>
   );
